4f158d57783cdcf23a65fb8008a41d507df9b7fd
unknown <vivekshinde535@gmail.com>
2023-10-25 14:52:01 +0530

    Initial commit

---
+++ Prac2c/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Pract2c" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title></head>
+<body>
+<form id="form1" runat="server">
+<div align="center">
+<asp:Label ID="Label1" runat="server" Text="Enter value of a:"></asp:Label>
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+<asp:Label ID="Label2" runat="server" Text="Enter value of b:"></asp:Label>
+<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+<asp:Label ID="Label3" runat="server" Text="Enter value of c:"></asp:Label>
+<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+<asp:Button ID="Button1" runat="server" Height="45px"
+onclick="Button1_Click" Text="Button" Width="79px" />
+</div></form>
+</body>
+</html>
---
+++ Prac2c/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+public partial class Pract2c : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+    public class quadeqtn
+    {
+        public double d, x1, x2;
+        public string msg;
+        public quadeqtn(int a, int b, int c)
+        {
+            d = b * b - 4 * a * c;
+            if (d == 0)
+            {
+                x1 = -b / (2.0 * a);
+                x2 = x1;
+                msg = "Both the roots are equal<br>1st Root is:" + x1 + "<br>2nd Root is:" + x2 +
+                "<br>";
+            }
+            else if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                msg = "Both the roots are real and different<br>1st Root is:" + x1 + "<br>2nd Root is:" + x2 + " < br > ";
+            }
+            else
+            {
+                msg = "Roots are imaginary, No solution.";
+            }
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int a, b, c;
+        a = Int32.Parse(TextBox1.Text);
+        b = Int32.Parse(TextBox2.Text);
+        c = Int32.Parse(TextBox3.Text);
+        quadeqtn qe = new quadeqtn(a, b, c);
+        Response.Write(qe.msg);
+    }
+}
